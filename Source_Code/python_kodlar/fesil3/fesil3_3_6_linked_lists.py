01fd12d21329e7fa5beb01d9b775da9471f7184f
Shahriyar Rzayev <rzayev.sehriyar@gmail.com>
2017-10-26 02:55:43 +0400

    Single Linked List kodunu ve Node tipini elave etdim

--- Source_Code/python_kodlar/fesil3/fesil3_3_6_linked_lists.py
+++ Source_Code/python_kodlar/fesil3/fesil3_3_6_linked_lists.py
@@ -1,5 +1,4 @@
 class Node:
-
     # konstruktor
     def __init__(self, data=None, next_node=None):
         self.data = data
@@ -24,3 +23,27 @@ class Node:
     # əgər bir node sonrakına point edirsə, true qaytar
     def has_next(self):
         return self.next_node is not None
+
+
+class SinglyLinkedList:
+    # Birtərəfli bağlı list
+    def __init__(self, head=None):
+        self.head = head
+
+    def list_size(self):
+        current = self.head
+        count = 0
+        while current:
+            count += 1
+            current = current.get_next_node()
+        return count
+
+    def insert_at_beginning(self, data):
+        new_node = Node()
+        new_node.set_data(data)
+
+        if self.list_size() == 0:
+            self.head = new_node
+        else:
+            new_node.set_next(self.head)
+            self.head = new_node
