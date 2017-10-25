56975e80f3ba87247d16d1a157dc460f5f7251bf
Shahriyar Rzayev <rzayev.sehriyar@gmail.com>
2017-10-25 19:33:18 +0400

    Bağlı list üçün tipin deklarasiyası

---
+++ Source_Code/python_kodlar/fesil3/fesil3_3_6_linked_list_type_declaration.py
@@ -0,0 +1,26 @@
+class Node:
+
+    # konstruktor
+    def __init__(self, data=None, next_node=None):
+        self.data = data
+        self.next_node = next_node
+
+    # node-un data field-ini mənimsətmək  üçün metod
+    def set_data(self, data):
+        self.data = data
+
+    # node-un data field-ini almaq üçün metod
+    def get_data(self):
+        return self.data
+
+    # node-un növbəti field-ini mənimsətmək üçün metod
+    def set_next(self, next_node):
+        self.next_node = next_node
+
+    # node-un növbəti fiel-ini almaq üçün metod
+    def get_next_node(self):
+        return self.next_node
+
+    # əgər bir node sonrakına point edirsə, true qaytar
+    def has_next(self):
+        return self.next_node is not None
